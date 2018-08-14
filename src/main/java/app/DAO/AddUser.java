f68ad6f778755049f215aafd352109333b017aa6
Knouton <knoutmefis@gmail.com>
2018-08-14 06:39:41 +0500

    Update AddUser.java

--- src/main/java/app/DAO/AddUser.java
+++ src/main/java/app/DAO/AddUser.java
@@ -9,7 +9,9 @@ import java.sql.SQLException;
 
 public class AddUser {
     public void addUser(User user) {
-        String SQLcommand = ("INSERT INTO users (Name) VALUES ('" + user.getName() + "');");
+        String[] protectionFromSQLinjection = user.getName().split(" "); //Разделяем вводимую строку по пробелу для защиты от sql инъекции
+
+        String SQLcommand = ("INSERT INTO users (Name) VALUES ('" + protectionFromSQLinjection[0] + "');");
         //("INSERT INTO users (Name, password) VALUES ('" + user.getName() + "' , '" + user.getPassword()+"');")
         try (Connection connection = CreateConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(SQLcommand);
